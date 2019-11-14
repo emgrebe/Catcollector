736d58ac6a0b8b9dfb90f6c504bb4fcdfa79d4c7
Emily Landgrebe <emilandgrebe@gmail.com>
2019-11-14 10:30:08 -0600

    create about url

--- catcollector2/settings.py
+++ catcollector2/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'catcollector2.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'catcollector',
+        'NAME': 'catcollector2',
     }
 }
 
